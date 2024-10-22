f5ed01627fda3f07f20a3648dfc6d3f391a7bbb8
DanielFernandez05 <christian.fernandez.castaneda@cbtis89.edu.mx>
2024-10-22 08:28:47 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,13 @@
+<?php
+/* CBTIS 89
+Programa que contiene mis datos personales
+Christian Daniel Fernandez Castañeda
+3°A Programacion Matutino
+Durango Dgo.
+*/
+
+echo "mi nombre es Christian Daniel Fernandez";
+echo "<br>", "Estudio en el CBTIS 89";
+echo "<br>", "En tercer semestre de la carrera programación A Matutino";
+echo "<br>", "Vivo en la ciudad de Durango Dgo.";
+?>
