466a354debfdc03b596c5a4d2cd485aa76bf340e
DanielFernandez05 <christian.fernandez.castaneda@cbtis89.edu.mx>
2024-11-20 10:29:49 -0600

    Add files via upload

---
+++ UNIDAD 3/Examen3FernandezCastaneda.php
@@ -0,0 +1,18 @@
+<?php
+/*
+CBTIS89
+Programa que almacena el nombre de cuatro articulos en un arreglo llamado $productos y en otro arreglo multidimensional llamado $precios almacena los precios correspondientes de 3 meses, esto se hace por medio de filas y columnas. 
+Debes calcular por medio de ciclos el subtotal y promedio de cada producto y guardarlo en los arreglos $subtotal y $promedio según corresponda.
+Christian Daniel Fernandez Castañeda
+*/
+$productos = array("Vestido", "Pantalon", "Cinturon", "Camiseta");
+$precios = array(300, 200, 500, 800);
+$subtotal = array();
+$promedio = array();
+echo "TIENDA DE ROPA"
+for(j=0; j<2; j++){
+	
+}
+
+
+?> 
